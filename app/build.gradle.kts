b94e8fce72fa0037750cf27b036f5456d0c04262
sorawitPHX <114576982+sorawitPHX@users.noreply.github.com>
2024-02-22 10:04:56 +0700

    เพิ่ม Dependency และ Plugin #1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // Parcelable
+    id("kotlin-parcelize")
 }
 
 android {
@@ -50,6 +53,22 @@ android {
 }
 
 dependencies {
+    // Navigation, Android Life-Cycle
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    // Menu & Nevigate Menu
+    implementation ("androidx.compose.material:material-icons-extended")
+    implementation ("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
+    implementation("androidx.compose.foundation:foundation:1.6.2")
+    // ส่วนของการติดต่อกับ api
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10")
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
