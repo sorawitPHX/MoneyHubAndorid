7c16bca4b868bd29460ff231e4ef2cbaea489c8d
00Boommalone00 <143213512+00Boommalone00@users.noreply.github.com>
2024-02-27 05:39:08 +0700

    จัดทำปุ่ม ทำicon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.code.gson:gson:2.10")
 
-
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/example/moneyhubandorid/FinanceScreen.kt
+++ app/src/main/java/com/example/moneyhubandorid/FinanceScreen.kt
@@ -3,15 +3,25 @@
 package com.example.project_1
 
 import android.content.Context
+import android.widget.ImageButton
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material.icons.filled.Check
@@ -38,10 +48,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
+import com.example.moneyhubandorid.R
 import com.example.moneyhubandorid.Screen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -58,6 +73,7 @@ fun FinanceScreen() {
     var selectedScreen by remember {
         mutableStateOf(0) // or use mutableStateOf(0)
     }
+
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -65,12 +81,10 @@ fun FinanceScreen() {
     ){
         CenterAlignedTopAppBar(
             title = {
+
                  },
             actions = {
                 // Notificotions icon
-                Row (
-
-                ) {
                     IconButton(
                         onClick = {
                             Toast.makeText(contextForToast, "ยกเลิกการแก้ไข", Toast.LENGTH_SHORT)
@@ -102,7 +116,7 @@ fun FinanceScreen() {
                     ) {
                         Icon(Icons.Default.Check, contentDescription = "รายได้")
                     }
-                }
+
 
             },//end action
             colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
@@ -110,25 +124,210 @@ fun FinanceScreen() {
             )
         )
 
-//        NavigationBar {
-//            navigationItems.forEachIndexed { index, screen ->
-//                NavigationBarItem(
-//                    icon = { Icon(imageVector = screen.icon, contentDescription = null) },
-//                    label = { Text(text = screen.name) },
-//                    selected = (selectedScreen == index),
-//                    onClick = { //this if condition keeps only one screen in the back stack
-//                        if (navController.currentBackStack.value.size >= 2) {
-//                            navController.popBackStack()
-//                        }
-//                        selectedScreen = index
-//                        navController.navigate(screen.route)
-//                        Toast.makeText(contextForToast, screen.name, Toast.LENGTH_SHORT).show()
-//                    }
+
+Box(
+    modifier = Modifier.fillMaxWidth()
+
+)
+        Row (
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.padding(vertical = 16.dp)
+        ){
+
+
+            ExpenseIconButton(
+                iconRes = R.drawable.fast_food,
+                label = "อาหาร",
+                onClick = { /*TODO*/ }
+            )
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "รายวัน",
+                onClick = { /*TODO*/ }
+            )
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "การจราจร",
+                onClick = { /*TODO*/ }
+            )
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "ทางสังคม",
+                onClick = { /*TODO*/ }
+            )
+
+        }
+        Row (
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.padding(vertical = 16.dp)
+        ){
+
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "ที่อยู่อาศัย",
+                onClick = { /*TODO*/ }
+            )
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "ของขวัญ",
+                onClick = { /*TODO*/ }
+            )
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "สื่อสาร",
+                onClick = { /*TODO*/ }
+            )
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "เสื้อผ้า",
+                onClick = { /*TODO*/ }
+            )
+
+        }
+        Row {
+
+
+            ExpenseIconButton(
+                iconRes = R.drawable.ic_launcher_foreground,
+                label = "การตั้งค่า",
+                onClick = { /*TODO*/ }
+            )
+
+
+        }
+    }
+
+}
+
+@Composable
+fun ExpenseIconButton(
+    iconRes: Int,
+    label: String,
+    onClick: () -> Unit
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = Modifier.size(100.dp)
+    ) {
+        Box(
+            modifier = Modifier
+//                .border(
+//                    width = 1.dp,
+//                    color = Color.Gray.copy(alpha = 0.5f),
+//                    shape = RoundedCornerShape(10.dp)
 //                )
-//            }
-//        }
-
-
-
+                .padding(10.dp)
+                .size(120.dp)
+                .aspectRatio(1f),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                //horizontalAlignment = Alignment.CenterHorizontally // เพิ่ม horizontalAlignment เพื่อจัดให้เนื้อหาอยู่กึ่งกลางในแนวนอน
+            ) {
+                Image(
+                    painter = painterResource(iconRes),
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.size(40.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = label,
+                    fontSize = 15.sp,
+                    color = Color.Black,
+                )
+            }
+        }
     }
 }
+
+//    ConstraintLayout(
+//        modifier = Modifier.fillMaxSize()
+//    ) {
+//        val ( secondText, catImage) = createRefs()
+//
+//
+//
+//        Text(
+//            text = "We are cats!",
+//            style = TextStyle(Color.Black),
+//            fontSize = 30.sp,
+//            modifier = Modifier
+//                .padding(5.dp)
+//                .constrainAs(secondText) {
+//                    centerHorizontallyTo(parent)
+//                    top.linkTo(catImage.bottom)
+//                }
+//        )
+//
+//        Image(
+//            painter = painterResource(id = R.drawable.cartoon_cat),
+//            contentDescription = null,
+//            contentScale = ContentScale.Fit,
+//            modifier = Modifier
+//                .size(300.dp)
+//                .constrainAs(catImage) {
+//                    centerHorizontallyTo(parent)
+//                    top.linkTo(catImage.top)
+//                }
+//        )
+//
+//
+//    }
+//    ConstraintLayout(
+//        modifier = Modifier.fillMaxSize()
+//    ) {
+//        val (secondText, catImage) = createRefs()
+//
+//        Text(
+//            text = "We are cats!",
+//            style = TextStyle(Color.Black),
+//            fontSize = 30.sp,
+//            modifier = Modifier
+//                .padding(5.dp)
+//                .constrainAs(secondText) {
+//                    centerHorizontallyTo(parent)
+//                    top.linkTo(catImage.bottom)
+//                }
+//        )
+
+//        ImageButton(
+//            onClick = {
+//                Toast.makeText(contextForToast, "คุณกำลังคลิกที่รูปภาพ", Toast.LENGTH_SHORT).show()
+//            },
+//            content = {
+//                Image(
+//                    painter = painterResource(id = R.drawable.cartoon_cat),
+//                    contentDescription = null,
+//                    contentScale = ContentScale.Fit,
+//                    modifier = Modifier
+//                        .size(300.dp)
+//                )
+//                Text(
+//                    text = "กดที่รูปภาพ",
+//                    color = Color.White,
+//                    modifier = Modifier
+//                        .align(Alignment.BottomCenter)
+//                        .padding(8.dp)
+//                )
+//            },
+//            modifier = Modifier
+//                .size(300.dp)
+//                .constrainAs(catImage) {
+//                    centerHorizontallyTo(parent)
+//                    top.linkTo(parent.top)
+//                }
+//        )
+
+
+
+
+
