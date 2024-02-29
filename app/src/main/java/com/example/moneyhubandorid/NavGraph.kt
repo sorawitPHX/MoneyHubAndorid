00015cfbc3659b412b6c47c082287eacd55348ba
natanon2023 <142309775+natanon2023@users.noreply.github.com>
2024-02-29 22:58:44 +0700

    แก้ error NavGraph (แจ็กกี้)

--- app/src/main/java/com/example/moneyhubandorid/NavGraph.kt
+++ app/src/main/java/com/example/moneyhubandorid/NavGraph.kt
@@ -5,7 +5,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.moneyhubandorid.screen.AnalysisScreen
-import com.example.moneyhubandorid.screen.FinanceScreen
+import com.example.moneyhubandorid.screen.Finance.Espense
+import com.example.moneyhubandorid.screen.Finance.FinanceScreen
+import com.example.moneyhubandorid.screen.Finance.income
 import com.example.moneyhubandorid.screen.HomeScreen
 import com.example.moneyhubandorid.screen.LoginScreen
 import com.example.moneyhubandorid.screen.ProfileScreen
